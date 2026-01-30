e81d40d737f8cc81882f52028e9af96d3664ded6
Mr-Code-start <vitalybaca92@gmail.com>
2026-01-30 05:56:21 -0500

    feat(): init operaciones

---
+++ operaciones.py
@@ -0,0 +1,12 @@
+a = 5
+b = 2
+
+suma = a + b
+resta = a - b
+multiplicacion = a * b
+division = a / b
+
+print("Suma:", suma)
+print("Resta:", resta)
+print("Multiplicación:", multiplicacion)
+print("División:", division)
