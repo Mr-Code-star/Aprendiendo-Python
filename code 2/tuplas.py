edac0788d82071eb6f63c08138d86a3cdc0d49ec
Mr-Code-start <vitalybaca92@gmail.com>
2026-01-30 06:44:06 -0500

    feat: init tuplas

---
+++ code 2/tuplas.py
@@ -0,0 +1,10 @@
+coordenadas = (3,5)
+print(coordenadas[0])
+
+# coordenadas[0] = 10  # Esto generará un error porque las tuplas son inmutables
+
+# las tuplas son útiles para datos que no deben cambiar
+persona = ("Alice", 30, "Ingeniera")
+print("Nombre:", persona[0])
+print("Edad:", persona[1])
+print("Profesión:", persona[2])
