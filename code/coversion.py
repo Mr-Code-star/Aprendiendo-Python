8bc36e22992e22ac8807075107bdb9aece6c223c
Mr-Code-start <vitalybaca92@gmail.com>
2026-01-30 06:07:11 -0500

    feat: init coversion

---
+++ code/coversion.py
@@ -0,0 +1,9 @@
+num_str = "100"
+num_int = int(num_str) # Convert string to int
+
+print(num_int, type(num_int))
+
+decimal = 99.99
+decimal_str = str(decimal) # Convert float to string
+
+print(decimal_str, type(decimal_str))
