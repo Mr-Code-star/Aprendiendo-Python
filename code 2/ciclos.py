10989e23105797cb01bebd4ec4658fec161876fb
Mr-Code-start <vitalybaca92@gmail.com>
2026-01-30 06:51:51 -0500

    feat: ciclos

---
+++ code 2/ciclos.py
@@ -0,0 +1,18 @@
+# Ciclo For
+# Definicion
+# El ciclo for se utiliza para iterar sobre una secuencia (como una lista, tupla, diccionario, conjunto o cadena de texto).
+frutas = ["manzana", "banana", "cereza"]
+
+for fruta in frutas:
+    print("Me gusta:", fruta)
+
+for i in range(5):
+    print("Número:", i)
+
+# Ciclo While
+# Definicion
+# El ciclo while se ejecuta mientras una condición sea verdadera.
+contador = 1
+while contador <= 3:
+    print("intento:", contador)
+    contador += 1    
